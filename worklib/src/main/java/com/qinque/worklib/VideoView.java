75cab6737e5204cc70c9adbaa2d04004999b8d37
dongdong <84494916@qq.com>
2023-06-27 17:43:36 +0800

    add

---
+++ worklib/src/main/java/com/qinque/worklib/VideoView.java
@@ -0,0 +1,21 @@
+package com.qinque.worklib;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+import androidx.annotation.Nullable;
+
+public class VideoView extends LinearLayout {
+    public VideoView(Context context) {
+        super(context);
+    }
+
+    public VideoView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public VideoView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
